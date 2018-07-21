845b19959c1563e39b82d35381b22d212a2ced29
stephen <stephen.wachanga@bituls.com>
2018-07-21 09:36:18 -0500

    add the item query function instead of erpnext controller

--- terester_restaurant/terester_restaurant/doctype/restaurant_order_entry/restaurant_order_entry.py
+++ terester_restaurant/terester_restaurant/doctype/restaurant_order_entry/restaurant_order_entry.py
@@ -6,7 +6,8 @@ from __future__ import unicode_literals
 import frappe, json
 from frappe.model.document import Document
 from frappe import _
-from erpnext.controllers.queries import item_query
+from frappe.desk.reportview import get_match_cond, get_filters_cond
+from frappe.utils import nowdate
 
 class RestaurantOrderEntry(Document):
 	pass
@@ -87,3 +88,46 @@ def get_restaurant_and_menu_name(table):
 		frappe.throw(_('Please set an active menu for Restaurant {0}').format(restaurant))
 
 	return restaurant, menu
+
+def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=False):
+	conditions = []
+
+	description_cond = ''
+	if frappe.db.count('Item') < 50000:
+		# scan description only if items are less than 50000
+		description_cond = 'or tabItem.description LIKE %(txt)s'
+
+	return frappe.db.sql("""select tabItem.name,
+		if(length(tabItem.item_name) > 40,
+			concat(substr(tabItem.item_name, 1, 40), "..."), item_name) as item_name,
+		tabItem.item_group,
+		if(length(tabItem.description) > 40, \
+			concat(substr(tabItem.description, 1, 40), "..."), description) as decription
+		from tabItem
+		where tabItem.docstatus < 2
+			and tabItem.has_variants=0
+			and tabItem.disabled=0
+			and (tabItem.end_of_life > %(today)s or ifnull(tabItem.end_of_life, '0000-00-00')='0000-00-00')
+			and (tabItem.`{key}` LIKE %(txt)s
+				or tabItem.item_group LIKE %(txt)s
+				or tabItem.item_name LIKE %(txt)s
+				or tabItem.barcode LIKE %(txt)s
+				{description_cond})
+			{fcond} {mcond}
+		order by
+			if(locate(%(_txt)s, name), locate(%(_txt)s, name), 99999),
+			if(locate(%(_txt)s, item_name), locate(%(_txt)s, item_name), 99999),
+			idx desc,
+			name, item_name
+		limit %(start)s, %(page_len)s """.format(
+		key=searchfield,
+		fcond=get_filters_cond(doctype, filters, conditions).replace('%', '%%'),
+		mcond=get_match_cond(doctype).replace('%', '%%'),
+		description_cond = description_cond),
+		{
+			"today": nowdate(),
+			"txt": "%%%s%%" % txt,
+			"_txt": txt.replace("%", ""),
+			"start": start,
+			"page_len": page_len
+		}, as_dict=as_dict)
